7e64b04efa14395ec04acff4c6d107c8bf85e953
Juliano Ribeiro <supernoi@msn.com>
2026-03-03 20:07:43 -0300

    ci: harden sqlfluff gate, remove || true, add .sqlfluff config and .prettierignore (#41)

--- databaseProject/fix_django_migrations.sql
+++ databaseProject/fix_django_migrations.sql
@@ -4,13 +4,13 @@ Execute este script para resolver problemas de migração
 */
 
 -- Verificar migrações existentes
-SELECT * FROM DJANGO_MIGRATIONS WHERE app = 'coreRelback' ORDER BY id;
+SELECT * FROM DJANGO_MIGRATIONS WHERE APP = 'coreRelback' ORDER BY ID;
 
 -- Marcar migrações como aplicadas (se necessário)
-INSERT INTO DJANGO_MIGRATIONS (app, name, applied) 
+INSERT INTO DJANGO_MIGRATIONS (APP, NAME, APPLIED)
 VALUES ('coreRelback', '0002_backuppolicy_client_database_host_relbackuser_and_more', CURRENT_TIMESTAMP);
 
-INSERT INTO DJANGO_MIGRATIONS (app, name, applied) 
+INSERT INTO DJANGO_MIGRATIONS (APP, NAME, APPLIED)
 VALUES ('coreRelback', '0005_add_user_preferences', CURRENT_TIMESTAMP);
 
 COMMIT;
